ca109101a4be0156c1e7374b0d6aff4d7cb72f3f
mikoavelli <mikoavelli@gmail.com>
2026-01-05 20:59:24 +0300

    feat(vscode): moved to official vscode binary

--- config.sh
+++ config.sh
@@ -84,7 +84,7 @@ AUR_PACKAGES=(
   morewaita-icon-theme       # A comprehensive flat icon theme style
   nautilus-open-any-terminal # Extension for nautilus, which allow opening alacritty from nautilus
   xdg-terminal-exec          # Default Terminal Execution Specification implementation
-  code-marketplace	     # Enable vscode marketplace in Code OSS
+  visual-studio-code-bin     # Visual Studio Code (vscode) official binaryvisual-studio-code-bin     
 )
 
 FLATPAK_PACKAGES=(
